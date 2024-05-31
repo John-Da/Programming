f6007f245ba0b9317e0c7a0b71d3ca56e8a2fdfb
MeeToo <john.213steven@gmail.com>
2024-05-31 23:39:52 +0700

    created reveiw box data js

---
+++ youtube/javaScriptKing/jvs/data/revBoxData.js
@@ -0,0 +1,15 @@
+export const testimonials = [
+    {
+        name: "Judy V",
+        photoUrl: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?w=1400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D",
+        text: "I have been using apple software for years now and I can confidently say that it has been a game changer for me. It has all the features I need and more, making my work and personal tasks much more efficient and organized. Thank you, Apple!"
+    },{
+        name: "Dame W",
+        photoUrl: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=1400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dXNlcnxlbnwwfHwwfHx8MA%3D%3D",
+        text: "I can confidently say that it has been a game changer for me. Thank you, Apple!"
+    },{
+        name: "Anne W",
+        photoUrl: "https://images.unsplash.com/photo-1499887142886-791eca5918cd?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjJ8fHVzZXJ8ZW58MHx8MHx8fDA%3D",
+        text: "Thank you, Apple!"
+    }
+];
