78934de616ece4955a20c91498241dbc948d7032
MeeToo <john.213steven@gmail.com>
2024-06-24 10:15:39 +0700

    data structure python example codes

--- programProject/p1.py
+++ programProject/p1.py
@@ -1,1 +1,34 @@
-print("Hello World")
+
+
+def fun1(n):
+    i = 0
+    if (n > 1):
+        fun1(n - 1)
+    for i in range(n):
+        print('*', end=' ')
+
+
+fun1(5)
+
+
+# def fun2(n):
+#     if n == 0:
+#         return 
+    
+#     fun2(n//2)
+#     print(n % 2, end="")
+
+
+# fun2(100)
+
+
+# def f(x):
+#     if x == 0:
+#         return 0
+#     else:
+#         return 2 * f(x-1) + x * x
+    
+
+
+# fn = float(input("Enter a number: "))
+# print("f(5) = ", f(fn))
