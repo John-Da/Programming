dc6e8f3970f3f5b97731a593a813904d74bc0710
MeeToo <john.213steven@gmail.com>
2024-06-26 20:42:55 +0700

    charactor facing

--- youtube/street fighter/charactor.py
+++ youtube/street fighter/charactor.py
@@ -3,14 +3,14 @@ import pygame
 
 class Fighter:
 
-
     def __init__(self, x, y):
+        self.flip = False
         self.rect = pygame.Rect((x, y, 80, 180))
         self.vel_y = 0
         self.jump = False
         self.attacking = False
         self.attack_type = 0
-
+        self.health = 100
 
     def move(self, screen_width, screen_height, surface, target):
         SPEED = 10
@@ -35,7 +35,6 @@ class Fighter:
                 if key[pygame.K_t]:
                     self.attack_type = 2
 
-
         self.vel_y += GRAVITY
         dy += self.vel_y
         if self.rect.left + dx < 0:
@@ -46,19 +45,26 @@ class Fighter:
             self.vel_y = 0
             self.jump = False
             dy = screen_height - 110 - self.rect.bottom
+
+        if target.rect.centerx > self.rect.centerx:
+            self.flip = False
+        else:
+            self.flip = True
         self.rect.x += dx
         self.rect.y += dy
-
 
     def attack(self, surface, target):
         self.attacking = True
         attacking_rect = pygame.Rect(
-            self.rect.centerx, self.rect.y, 2 * self.rect.width, self.rect.height
+            self.rect.centerx - (2 * self.rect.width * self.flip),
+            self.rect.y,
+            2 * self.rect.width,
+            self.rect.height,
         )
         if attacking_rect.colliderect(target.rect):
-            print("hit")
-        pygame.draw.rect(surface, (0, 255, 0), attacking_rect)
+            target.health -= 10
 
+        pygame.draw.rect(surface, (0, 255, 0), attacking_rect)
 
     def draw(self, surface):
         pygame.draw.rect(surface, (255, 0, 0), self.rect)
--- youtube/street fighter/fighting.py
+++ youtube/street fighter/fighting.py
@@ -19,12 +19,23 @@ pygame.display.set_caption(game_name)
 clock = pygame.time.Clock()
 FPS = 60
 
+
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
 bg_image = pygame.image.load(background).convert_alpha()
 
 
 def draw_bg():
     scaled_bg = pygame.transform.scale(bg_image, (screenWidth, screenHeight))
     screen.blit(scaled_bg, (0, 0))
+
+
+def draw_health_bar(health, x, y):
+    ratio = health / 100
+    pygame.draw.rect(screen, WHITE, (x - 2, y - 2 , 404, 34))
+    pygame.draw.rect(screen, RED,(x, y, 400, 30))
+    pygame.draw.rect(screen, YELLOW, (x, y, 400 * ratio, 30))
 
 
 fighter_1 = Fighter(200, 310)
@@ -36,6 +47,10 @@ while run:
     clock.tick(FPS)
 
     draw_bg()
+    draw_health_bar(fighter_1.health, 20, 20)
+    draw_health_bar(fighter_2.health, 580, 20)
+
+
     fighter_1.move(screenWidth, screenHeight, screen, fighter_2)
     # fighter_2.move(screenWidth,screenHeight)
     fighter_1.draw(screen)
