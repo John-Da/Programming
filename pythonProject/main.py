24dd9997542e547a66c7689345a83e5f6fcf2821
Mee Too <john.213steven@gmail.com>
2024-07-06 16:36:44 +0700

    game

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,6 +1,6 @@
 import pygame
 
-from image_store import bgSky, bgGrd, gameFont1, snail1
+from image_store import bgSky, bgGrd, gameFont1, snail1,playWalk1
 
 pygame.init()
 
@@ -22,9 +22,17 @@ timer_surface = timer_font.render('Timer: ', False, 'black')
 
 sky_surface = pygame.image.load(bgSky).convert()
 ground_surface = pygame.image.load(bgGrd).convert()
-snail_surface = pygame.image.load(snail1).convert_alpha()
+
 snail_posX = 600
-snail_posY = 264
+snail_posY = 300
+snail_surface = pygame.image.load(snail1).convert_alpha()
+snail_rect = snail_surface.get_rect(bottomright = (snail_posX, snail_posY))
+
+
+player_surface = pygame.image.load(playWalk1).convert_alpha()
+player_rect = player_surface.get_rect(midbottom = (80, 300))
+
+
 
 
 runLoop = True
@@ -36,12 +44,14 @@ while runLoop:
 
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0,300))
+
     screen.blit(score_surface, (15, 10))
     screen.blit(timer_surface, (620, 10))
-    snail_posX -= 2
-    if snail_posX < -80:
-        snail_posX = 800
-    screen.blit(snail_surface, (snail_posX, snail_posY))
+
+    snail_rect.x -= 2
+    if snail_rect.right <= 0: snail_rect.left = 800
+    screen.blit(snail_surface, snail_rect)
+    screen.blit(player_surface, player_rect)
 
     pygame.display.update()
     clock.tick(60)
