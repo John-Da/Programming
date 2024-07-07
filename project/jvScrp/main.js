5bfb81de5158e8e1d331c8de4611d3c246186833
Mee Too <john.213steven@gmail.com>
2024-07-07 22:24:42 +0700

    function start

---
+++ project/jvScrp/main.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+var a = 60;
+var b = a;
+var t = setInterval(() => {
+    var s = document.getElementById('timer');
+    s.style.strokeDashoffset = -(450 - a) / b;
+    a = a < 10 ? '0' + a: a;
+    document.getElementById('timer').innerHTML = a;
+    setTimeout(() => {
+        s.style.transition = '2s';
+    }, 10);
+    a--;
+    if (a == -1){
+        clearInterval(t);
+    }
+}, 1000);
