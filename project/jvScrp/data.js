b308b96ae76511fabb83e5bf10f3a4a47122ebbc
Mee Too <john.213steven@gmail.com>
2024-07-08 23:18:57 +0700

    function

--- project/jvScrp/data.js
+++ project/jvScrp/data.js
@@ -1,4 +1,4 @@
-export const countries = [
+export var countries = [
     
     {
         name: 'Argentina',
--- project/jvScrp/gameBoard.js
+++ project/jvScrp/gameBoard.js
@@ -1,24 +1,55 @@
-import {countries} from '../jvScrp';
+var images = [
+    {src: 'https://via.placeholder.com/200?text=Image1', name: 'Image 1'},
+    {src: 'https://via.placeholder.com/200?text=Image2', name: 'Image 2'},
+    {src: 'https://via.placeholder.com/200?text=Image3', name: 'Image 3'},
+    {src: 'https://via.placeholder.com/200?text=Image4', name: 'Image 4'}
+];
+
+var score = 0;
+
+// Function to shuffle an array
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+// Function to display a new random image and buttons
+function displayNewImage() {
+    // Select a random image
+    var randomImage = images[Math.floor(Math.random() * images.length)];
+
+    // Display the image
+    var imageContainer = document.getElementById('imageContainer');
+    imageContainer.innerHTML = ''; // Clear previous image
+    var imgElement = document.createElement('img');
+    imgElement.src = randomImage.src;
+    imageContainer.appendChild(imgElement);
+
+    // Shuffle and create buttons
+    var buttonContainer = document.getElementById('buttonContainer');
+    buttonContainer.innerHTML = ''; // Clear previous buttons
+    var buttonNames = images.map(img => img.name);
+    buttonNames = shuffle(buttonNames);
+    for (var i = 0; i < buttonNames.length; i++) {
+        var button = document.createElement('button');
+        button.innerHTML = buttonNames[i];
+        button.addEventListener('click', function() {
+            if (this.innerHTML === randomImage.name) {
+                score++;
+            } else {
+                score = score;
+            }
+            document.getElementById('score').innerText = 'Score: ' + score;
+            displayNewImage(); // Display a new image and buttons after clicking
+        });
+        buttonContainer.appendChild(button);
+    }
+}
+
+// Initial call to display the first image and buttons
+displayNewImage();
 
 
-let gameBoard = '';
-
-countries.forEach((country) => {
-    gameBoard += 
-    `
-        <div class="content-left">
-            <div class="image">
-                <img src="${item.image}" id="image">
-            </div>
-        </div>
-
-        <div class="content-right">
-            <div class="question">
-                <h2>What country is this?</h2>
-            </div>
-            <div class="btn" id="btn_ans">
-                <button class="ans_btn" id="ans_1" data-image-id="${item.id}">${item.name}</button>
-            </div>
-        </div>
-    `
-}) 
