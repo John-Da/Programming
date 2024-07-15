f542f9e06f89fff40f04d47d10a64817e3196235
Mee Too <john.213steven@gmail.com>
2024-07-15 19:05:37 +0700

    school

--- schoolProject/assign/Javascript/JavaScript_Level_One/Part9_answer.js
+++ schoolProject/assign/Javascript/JavaScript_Level_One/Part9_answer.js
@@ -2,12 +2,10 @@ var fname = prompt("First Name: ")
 var lname = prompt("Last Name: ")
 var age = prompt("Age: ")
 var height = prompt("Height (cm): ")
-var pet = prompt("Pet: ")
-
-let conditionCheck = false
+var pet = prompt("Pet Name: ")
 
 if (fname[0] === lname[0] && (age > 20 && age < 30) && (height >= 170) && (pet[pet.length -1].toLowerCase() === 'y')){
-    console.log('Alert: message!')
+    console.log('Alert: Found the person!')
 }
 else{
     console.log('Not found yet!')
